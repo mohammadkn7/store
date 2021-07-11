861d0813d060c0abbbc0ef61cf08c0ce780822e5
mohammad kashani <me_kashani@yahoo.com>
2021-07-11 10:52:57 +0430

    genarate component & config routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'sign-up', component: UserRegisterComponent },
+  { path: 'not-found', component: ErrorComponent, data: {message: 'Page not found!'} },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './header/header.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    HeaderComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
