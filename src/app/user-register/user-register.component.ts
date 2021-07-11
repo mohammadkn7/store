b34ecdb25094ddbe9a17ca7a013af67c0c5bdb6a
mohammad kashani <me_kashani@yahoo.com>
2021-07-11 16:58:29 +0430

    validat sign up form complete

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -8,7 +8,7 @@ import { ValidatorsService } from './validators.service';
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.scss'],
   providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: true}
   }]
 })
 export class UserRegisterComponent implements OnInit {
@@ -18,20 +18,31 @@ export class UserRegisterComponent implements OnInit {
   constructor(private validatorService: ValidatorsService) { }
 
   ngOnInit() {
-    this.signUpForm = new FormGroup({      
-      'name': new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),  // name 10-100
-      'lastName': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(100)]), // lastName 10-100
-      'email': new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(150)] ), // email max = 150   
-      'birthday': new FormControl(null, [Validators.required] ), 
-      'address': new FormControl(null, [Validators.required, Validators.minLength(20), Validators.maxLength(200)]),  // name 20-200
-      'zipCode': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),  // zip-code 10 
+    this.signUpForm = new FormGroup({     
+      
+      'userInfo': new FormGroup({
+        'name': new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),  // name 10-100
+        'lastName': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(100)]), // lastName 10-100
+        'email': new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(150)] ), // email max = 150   
+        'birthday': new FormControl(null, [Validators.required] ), 
+      }),
+      'addressInfo': new FormGroup({
+        'address': new FormControl(null, [Validators.required, Validators.minLength(20), Validators.maxLength(200)]),  // name 20-200
+        'zipCode': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), this.validatorService.zipCodeValidator]),  // zip-code 10 
+      }),
       'password': new FormControl("null", [Validators.required, Validators.minLength(10),this.validatorService.passwordValidator]),  // password min 10 char and must contain at least 1 uppercase  character 
-      'rePassword': new FormControl(null, [Validators.required, Validators.minLength(10)]) 
+      'rePassword': new FormControl(null, [Validators.required, Validators.minLength(10),this.validatorService.confiremValidator]) 
       });
   }
 
-  onsubmite() {
-    console.log(this.signUpForm.controls.password.status);
-    console.log(this.signUpForm.controls.password);
+  onsubmit() {
+    console.log(2332);
+    // console.log(this.signUpForm.controls.password.status);
+   
+      // console.log(this.signUpForm.controls.userInfo.email.touched);
+      console.log(this.signUpForm.get('userInfo.email'));
+    
+    // console.log(this.signUpForm.controls.adressInfo.status);
+    
   }
 }
--- src/app/user-register/validators.service.ts
+++ src/app/user-register/validators.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class ValidatorsService {
   constructor() { }
 
   passwordValidator(control:any) {    
-    //      - password must contain at least 1 uppercase  character 
+    // - password must contain at least 1 uppercase  character 
     
     if(control.value) {
       if (control.value.match(/([A-Z])/)) {
@@ -20,4 +21,30 @@ export class ValidatorsService {
     return null
   }
 
+  zipCodeValidator(control:any) {
+    // console.log(control);
+    // console.log(control.get('password').value);
+    if(control.value) {
+      if (control.value.match(/^[0-9]*$/)) {
+        return null;
+      } else {
+        return { 'onlyNumeric': true };
+      }
+    }
+    return null
+  }
+
+  confiremValidator(control: any) {
+
+    console.log("console confirm", control.parent);
+    if(control.value) {
+
+      if (control.value == control.parent.value.password) {
+        return null;
+      } else {
+        return { 'passwordNotConfirm': true };
+      }
+    }
+    return null
+  }
 }
