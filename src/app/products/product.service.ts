c679940d7d39f1c90e8d84005b8e1f67dd4c36fc
mohammad kashani <me_kashani@yahoo.com>
2021-07-14 13:05:48 +0430

    change product

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -10,6 +10,8 @@ export class ProductService {
  
   products: Product[] = [
     new Product (0, 'https://picsum.photos/1000/1000?random=1', 'London', 1200000),
+    new Product (1, 'https://image.shutterstock.com/image-photo/tehraniranfamous-night-view-tehranflow-traffic-260nw-1520878961.jpg', 'Tehran', 110000),
+
     new Product (2, 'https://picsum.photos/1000/1000?random=3', 'Tokyo', 1500000),
     new Product (3, 'https://picsum.photos/1000/1000?random=4', 'Leeds', 1100000),
     new Product (4, 'https://picsum.photos/1000/1000?random=5', 'Paris', 100000),
@@ -20,7 +22,6 @@ export class ProductService {
     new Product (9, 'https://picsum.photos/1000/1000?random=10', 'Milan', 90000),
     new Product (10, 'https://picsum.photos/1000/1000?random=11', 'Barcelona', 500000),
     new Product (11, 'https://picsum.photos/1000/1000?random=12', 'Berlin', 1400000),
-    new Product (1, 'https://image.shutterstock.com/image-photo/tehraniranfamous-night-view-tehranflow-traffic-260nw-1520878961.jpg', 'Tehran', 50000),
 
   ];
  
